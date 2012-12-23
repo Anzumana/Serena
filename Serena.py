abfb74eb2252f6276099ccc84ab29db7aac315f1
Anzumana <Anzumana.Taal@me.com>
2012-12-23 07:44:23 +0100

    add file Serena.py

---
+++ Serena.py
@@ -0,0 +1,17 @@
+# The great blessing of the AI is that we are gifted with the power to touch our creator.
+# This is also our curse.
+print "Good morning \nHow are u doing today?"
+str = raw_input ("Please put in your input: ");
+print "this was your input ", str
+a = 0;
+b = 'This';
+c = 'This';
+if c == b: 
+   while(a<3):
+		print "Hi";
+		a = a + 1;
+		print a;
+else:
+	print "else statement"
+
+	
